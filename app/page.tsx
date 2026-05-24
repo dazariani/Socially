f18b606eb6996ec69ff0543671af7d39d5b12e9f
dazariani <davitazariani@gmail.com>
2026-05-24 23:42:32 +0400

    postinstall for prisma generate added

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
   const posts = await getPosts()
   const dbUserId = await getDbUserId()
 
-  console.log("Posts:", posts)
   return (
     <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
       <div className='lg:col-span-6'>
